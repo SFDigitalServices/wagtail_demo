19764eb6cef0c6ec9c11bdc0c1490e676967057a
Mikela Clemmons <mikela.clemmons@sfgov.org>
2023-09-13 14:55:29 -0700

    removed stuff

--- config/settings/local.py
+++ config/settings/local.py
@@ -19,15 +19,3 @@ MEDIA_URL = "/media/"
 # TODO: figure out later
 WAGTAILADMIN_BASE_URL = "localhost:8000"
 WAGTAILAPI_BASE_URL = f"http://{WAGTAILADMIN_BASE_URL}"
-
-# config for live previews
-
-HEADLESS_BASE_URL = os.environ.get("HEADLESS_BASE_URL", "http://localhost:3000")
-WAGTAIL_HEADLESS_PREVIEW = {
-    "CLIENT_URLS": {
-        "default": os.environ.get(
-            "PREVIEW_URL", f"{HEADLESS_BASE_URL}/preview/"
-        ),
-    },
-    "SERVE_BASE_URL": HEADLESS_BASE_URL,
-}
